7e1b7a59c632a485ae9198a6ff120210d8cae906
Olexiy <shevchuka@gmail.com>
2017-06-02 16:08:07 +0300

    fix

--- routes/api.php
+++ routes/api.php
@@ -2,12 +2,8 @@
 
 Route::group(['prefix' => '/v1', 'namespace' => 'Api\V1', 'as' => 'api.'], function () {
 
-        Route::resource('maps', 'MapsController');
+    Route::resource('maps', 'MapsController');
 
-        Route::resource('localized_maps', 'LocalizedMapsController');
-
-        Route::resource('actions', 'ActionsController');
-
-        Route::resource('localized_actions', 'LocalizedActionsController');
+    Route::get('languages', 'LanguagesController@index');
 
 });
