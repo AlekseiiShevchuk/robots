ae3f3ca1d85f7eeec7c4bfc79ff7c2b4054203d6
Olexiy <shevchuka@gmail.com>
2017-06-06 11:49:15 +0300

    convert languages for frontend needs

--- app/Http/Controllers/Api/V1/LanguagesController.php
+++ app/Http/Controllers/Api/V1/LanguagesController.php
@@ -5,16 +5,33 @@ namespace App\Http\Controllers\Api\V1;
 use App\Http\Controllers\Controller;
 use App\Language;
 use App\TranslationItem;
+use Illuminate\Database\Eloquent\Collection;
 
 class LanguagesController extends Controller
 {
 
     public function index()
     {
-        return [
-            'available_languages' => Language::where('is_active_for_users', 1)->get()->makeHidden(['created_at','updated_at','is_active_for_admin', 'is_active_for_users']),
-            'translations' => TranslationItem::all(Language::getAvailableColumnsForTranslationItems())
-            ];
+        $langCollection = new Collection();
+        $languages = Language::isActiveForUsers()->makeHidden([
+            'created_at',
+            'updated_at',
+            'is_active_for_admin',
+            'is_active_for_users'
+        ]);
+        $translations = TranslationItem::all();
+
+        // convert Models for frontend needs
+        foreach ($languages as $language) {
+            $item = ['language' => $language->abbreviation];
+            foreach ($translations as $translation) {
+                $value = 'value_' . $language->abbreviation;
+                $item['translations'][] = [$translation->value_name => $translation->$value];
+            }
+            $langCollection->add($item);
+        }
+
+        return $langCollection;
     }
 
 }
